6ae5c2bf4983b77828bfd510e2bf953af4d89098
saraBiton <sa8661117@gmail.com>
2023-06-04 20:58:03 +0300

    עכעכ

--- client/components/Users/table2.js
+++ client/components/Users/table2.js
@@ -3,120 +3,26 @@ import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
 // import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-reanimated-table';
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 import { getAll } from '../../src/Service';
+import { wrap } from 'lodash';
 
-// const Table2 = () => {
-// 	const head = [
-// 		'Full name',
-// 		'City',
-// 		'Role',
-// 		'', ''
-// 	];
-
-// 	const [tableData, setTableData] = useState({
-// 		keys: head,
-// 		values: [[], [], []]
-// 	});
-
-// 	useEffect(() => { makeTable(); }, []);
-
-// 	const styles = StyleSheet.create({
-// 		container: {
-// 			marginTop: 30,
-// 			marginHorizontal: 'auto'
-// 		},
-// 		headStyle: {
-// 			margin: 10,
-// 			textAlign: 'center',
-// 			fontWeight: 'bold'
-// 		},
-// 		textStyle: {
-// 			margin: 10,
-// 			textAlign: 'center',
-// 			fontWeight: 'normal'
-// 		},
-
-// 		button: {
-// 			margin: 5,
-// 			padding: 5,
-// 			backgroundColor: 'gray',
-// 			borderRadius: 10
-// 		},
-// 		buttonText: {
-// 			color: 'white',
-// 			margin: 'auto'
-// 		}
-// 	});
-
-// 	return (
-// 		<View style={styles.container}>
-// 			<TouchableOpacity style={[styles.button, { marginBottom: 20 }]}>
-// 				<Text style={styles.buttonText}>Add user</Text>
-// 			</TouchableOpacity>
-// 			<Table borderStyle={{ borderWidth: 2 }} >
-// 				<Row data={tableData.keys} textStyle={styles.headStyle} />
-// 				<Rows data={tableData.values} textStyle={styles.textStyle} />
-// 			</Table>
-// 		</View>
-// 	);
-
-// 	async function makeTable () {
-// 		function EditDeleteButtons () {
-// 			return (
-// 				<Fragment>
-
-// 					<TouchableOpacity style={styles.button}>
-// 						<Text style={styles.buttonText}>edit</Text>
-// 					</TouchableOpacity>
-
-// 					<TouchableOpacity style={styles.button}>
-// 						<Text style={styles.buttonText}>delete</Text>
-// 					</TouchableOpacity>
-
-// 				</Fragment>
-// 			);
-// 		};function ShowSensorsButtons () {
-// 			return (
-// 				<TouchableOpacity style={styles.button}>
-// 					<Text style={styles.buttonText}>show sensors</Text>
-// 				</TouchableOpacity>
-// 			);
-// 		};
-
-// 		const r = await getAll().then(res => res.data);
-
-// 		const keys = head;
-
-// 		const values = r.reduce((array, value) => {
-// 			const rowValues = Object.values(value);
-// 			rowValues.push(<EditDeleteButtons/>);
-// 			rowValues.push(<ShowSensorsButtons/>);
-// 			array.push(rowValues);
-// 			return array;
-// 		}, []);
-
-// 		setTableData({
-// 			keys, values
-// 		});
-// 	}
-// };
 const Table2 = () => {
-	const head = ['Full name', 'City', 'Role', '', ''];
+	const head = [
+		'Full name',
+		'City',
+		'Role',
+		'', ''
+	];
 
 	const [tableData, setTableData] = useState({
 		keys: head,
 		values: [[], [], []]
 	});
 
-	useEffect(() => {
-		makeTable();
-	}, []);
+	useEffect(() => { makeTable(); }, []);
 
 	const styles = StyleSheet.create({
-		table: {
-			backgroundColor: 'red',
-			borderWidth: 2
-		},
 		container: {
+			flexWrap: wrap,
 			marginTop: 30,
 			marginHorizontal: 'auto'
 		},
@@ -148,7 +54,7 @@ const Table2 = () => {
 			<TouchableOpacity style={[styles.button, { marginBottom: 20 }]}>
 				<Text style={styles.buttonText}>Add user</Text>
 			</TouchableOpacity>
-			<Table style={styles.table}>
+			<Table borderStyle={{ borderWidth: 2 }} >
 				<Row data={tableData.keys} textStyle={styles.headStyle} />
 				<Rows data={tableData.values} textStyle={styles.textStyle} />
 			</Table>
@@ -156,17 +62,10 @@ const Table2 = () => {
 	);
 
 	async function makeTable () {
-		const ShowSensorsButtons = () => {
+		function EditDeleteButtons () {
 			return (
-				<TouchableOpacity style={styles.button}>
-					<Text style={styles.buttonText}>show sensors</Text>
-				</TouchableOpacity>
-			);
-		};
+				<Fragment>
 
-		const EditDeleteButtons = () => {
-			return (
-				<>
 					<TouchableOpacity style={styles.button}>
 						<Text style={styles.buttonText}>edit</Text>
 					</TouchableOpacity>
@@ -174,27 +73,35 @@ const Table2 = () => {
 					<TouchableOpacity style={styles.button}>
 						<Text style={styles.buttonText}>delete</Text>
 					</TouchableOpacity>
-				</>
+
+				</Fragment>
+			);
+		};function ShowSensorsButtons () {
+			return (
+				<TouchableOpacity style={styles.button}>
+					<Text style={styles.buttonText}>show sensors</Text>
+				</TouchableOpacity>
 			);
 		};
 
-		const r = await getAll().then((res) => res.data);
+		const r = await getAll().then(res => res.data);
 
 		const keys = head;
 
 		const values = r.reduce((array, value) => {
 			const rowValues = Object.values(value);
-			rowValues.push(<EditDeleteButtons />);
-			rowValues.push(<ShowSensorsButtons />);
+			rowValues.push(<EditDeleteButtons/>);
+			rowValues.push(<ShowSensorsButtons/>);
 			array.push(rowValues);
 			return array;
 		}, []);
 
 		setTableData({
-			keys,
-			values
+			keys, values
 		});
 	}
 };
 
-export { Table2 };
+export {
+	Table2
+};
