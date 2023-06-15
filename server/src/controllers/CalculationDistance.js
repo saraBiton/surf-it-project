e3e302d4f4c0dc716a20bff947214ea700ab1082
saraBiton <sa8661117@gmail.com>
2023-06-15 04:29:10 +0300

    1

--- server/src/controllers/CalculationDistance.js
+++ server/src/controllers/CalculationDistance.js
@@ -1,29 +1,64 @@
+//מקבלת מקור ויעדים ומחזירה את המרחק בין המקור לכל יעד
+//באמצעות שימוש ב-API של Google Maps
+//המרחקים מוחזרים בפורמט JSON.
+
+// async function getDistance(origin, destinations) {
+//   const apiKey = "AIzaSyBs28fQD8-yiY6leR2cAXSv9CGl5Sm4eVQ";
+//   const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${origin.lat},${origin.lng}&destinations=`;
+
+//   destinations.forEach((destination) => {
+//     url += `${destination.lat},${destination.lng}|`;
+//   });
+
+//   url += `&key=${apiKey}`;
+//   axios
+//     .get(url)
+//     .then((response) => {
+//       const data = response.data;
+//       const rows = data.rows[0].elements;
+
+//       rows.forEach((element, index) => {
+//         const destination = destinations[index];
+//         const travelTime = element.duration.text;
+//         console.log(
+//           `Travel time from origin to destination (${destination.lat},${destination.lng}): ${travelTime}`
+//         );
+//       });
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }
+
+import axios from "axios";
+import { User } from "../Models/userModel.js";
+import { Defibrillator } from "../Models/defibrillatorModel.js";
+
 async function getDistance(origin, destinations) {
-  const apiKey = "AIzaSyBs28fQD8-yiY6leR2cAXSv9CGl5Sm4eVQ";
-  const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${origin.lat},${origin.lng}&destinations=`;
+  try {
+    const apiKey = "YOUR_API_KEY";
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=${origin.lat},${origin.lng}&destinations=`;
 
-  destinations.forEach((destination) => {
-    url += `${destination.lat},${destination.lng}|`;
-  });
-
-  url += `&key=${apiKey}`;
-  axios
-    .get(url)
-    .then((response) => {
-      const data = response.data;
-      const rows = data.rows[0].elements;
-
-      rows.forEach((element, index) => {
-        const destination = destinations[index];
-        const travelTime = element.duration.text;
-        console.log(
-          `Travel time from origin to destination (${destination.lat},${destination.lng}): ${travelTime}`
-        );
-      });
-    })
-    .catch((error) => {
-      console.log(error);
+    destinations.forEach((destination) => {
+      url += `${destination.lat},${destination.lng}|`;
     });
+
+    url += `&key=${apiKey}`;
+    const response = await axios.get(url);
+    const data = response.data;
+    const rows = data.rows[0].elements;
+
+    const distances = {};
+    rows.forEach((element, index) => {
+      const destination = destinations[index];
+      const distance = element.distance.value;
+      distances[`${destination.lat},${destination.lng}`] = distance;
+    });
+
+    return distances;
+  } catch (error) {
+    throw new Error("Failed to get distances from Google Maps API");
+  }
 }
 
 const getActiveVolunteersDistances = async (point) => {
@@ -32,19 +67,35 @@ const getActiveVolunteersDistances = async (point) => {
   const activeVolunteers = volunteers
     .filter((v) => v.volunteer.isActive === true)
     .map((v) => {
-      return { lng: v.volunteer.lng, lat: v.volunteer.lat };
+      return { lng: v.volunteer.lng, lat: v.volunteer.lat, id };
     });
-
   console.log(activeVolunteers);
 
-  const distances = await getDistance(point, activeVolunteers);
+  const distancesVolunteers = await getDistance(point, activeVolunteers);
 
   console.log(distances);
 
-  const result = dijkstra(
+
+
+  const defibrillators = await Defibrillator.find();
+
+  const defibrillatorsfree = defibrillators
+    .filter((v) => d.Defibrillator.isActive === false)
+    .map((v) => {
+      return { lng: d.Defibrillator.lng, lat: d.Defibrillator.lat, id:d.Defibrillator.id };
+    });
+  console.log(activeVolunteers);
+
+  const distancesDefibrillators = await getDistance(point, defibrillatorsfree);
+
+  console.log(distances);
+
+
+
+  const result = dijkstraGraph(
     graph,
     point,
-    distances[0].destination,
+    distancesVolunteers[0].destination,
     "defibrillator2"
   );
 
--- server/src/controllers/dijkstraGraph.js
+++ server/src/controllers/dijkstraGraph.js
@@ -2,15 +2,15 @@ import PriorityQueue from "js-priority-queue";
 
 // define graph
 const graph = {
-  drowning: { defibrillator1: 1500, defibrillator2: 100 },
-  defibrillator1: { volunteer1: 1500, volunteer2: 500 },
+  drowning: { defibrillator1: 100, defibrillator2: 1000 },
+  defibrillator1: { volunteer1: 200, volunteer2: 500 },
   defibrillator2: { volunteer1: 500, volunteer2: 100 },
   volunteer1: {},
   volunteer2: {},
 };
 
 // define function to find shortest path
-export default function dijkstra(graph, source, target, defibrillator) {
+export default function dijkstraGraph(graph, source, target, defibrillator) {
   const distances = {}; // מפה של מרחקים מהצומת המקורי לכל צומת בגרף
   const previous = {}; // מפה של הצמתים הקודמים במסלול הקצר ביותר מהצומת המקורי לכל צומת בגרף.
   const queue = new PriorityQueue({
@@ -77,6 +77,65 @@ export default function dijkstra(graph, source, target, defibrillator) {
 }
 
 // find shortest path from drowning to nearest volunteer passing through defibrillator2
-const result = dijkstra(graph, "drowning", "volunteer2", "defibrillator2");
+const result = dijkstraGraph(graph, "drowning", "volunteer1", "defibrillator2");
 console.log(result.path); // output: ['drowning', 'defibrillator2', 'volunteer2']
 console.log(result.distance); // output: 2100
+
+
+//////////
+
+
+export default function findShortestPath(graph, sensor) {
+  const defibrillators = [];
+  const volunteers = [];
+
+  // Find all defibrillator and volunteer nodes
+  for (const node in graph) {
+    if (node.startsWith("defibrillator")) {
+      defibrillators.push(node);
+    } else if (node.startsWith("volunteer")) {
+      volunteers.push(node);
+    }
+  }
+
+  const shortestPaths = {}; // Store the shortest path and distance for each volunteer node
+
+  // Find the shortest path from each defibrillator to the sensor node
+  const sensorDistances = {};
+  for (const defibrillator of defibrillators) {
+    const result = dijkstraGraph(graph, defibrillator, sensor);
+    sensorDistances[defibrillator] = result.distance;
+  }
+
+  // Find the shortest path from each volunteer to the sensor passing through the defibrillator with the shortest path to the sensor
+  for (const volunteer of volunteers) {
+    let shortestDefibrillator = null;
+    let shortestDistance = Infinity;
+    for (const defibrillator of defibrillators) {
+      const result = dijkstraGraph(graph, volunteer, defibrillator);
+      const distance = result.distance + sensorDistances[defibrillator];
+      if (distance < shortestDistance) {
+        shortestDefibrillator = defibrillator;
+        shortestDistance = distance;
+        shortestPath = result.path;
+      }
+    }
+    shortestPath.push(sensor);
+    shortestPaths[volunteer] = {
+      path: shortestPath,
+      distance: shortestDistance,
+    };
+  }
+
+  // Find the shortest path and distance among all the paths
+  let shortestPath = null;
+  let shortestDistance = Infinity;
+  for (const volunteer in shortestPaths) {
+    if (shortestPaths[volunteer].distance < shortestDistance) {
+      shortestPath = shortestPaths[volunteer].path;
+      shortestDistance = shortestPaths[volunteer].distance;
+    }
+  }
+
+  return { path: shortestPath, distance: shortestDistance };
+}
